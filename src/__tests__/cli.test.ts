3b9740f306e54baa10bda4cb90a6c5ef460e890a
noatudor <143794128+noatudor@users.noreply.github.com>
2025-07-21 15:39:35 +0200

    Optimize MySQL import with performance tweaks and resets

--- src/__tests__/cli.test.ts
+++
@@ -1,7 +0,0 @@
-// Simple test file to verify exports
-describe('CLI', () => {
-  it('should be importable', () => {
-    // Just check that we can require the file without errors in normal operation
-    expect(true).toBe(true);
-  });
-});
